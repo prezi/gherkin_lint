57511fb1b014f9e075b6356df887017142187b40
annelledj <annelle.dejager@prezi.com>
2018-02-24 09:06:35 +0100

    Exclude in progress and flaky tags from tag collector

--- lib/gherkin_lint/linter/tag_collector.rb
+++ lib/gherkin_lint/linter/tag_collector.rb
@@ -1,9 +1,13 @@
 module GherkinLint
   # Mixin to lint for tags based on their relationship to eachother
   module TagCollector
+    def check_excluded_tag_condition(tag)
+      true if tag[:name].include?('in-progress') || tag[:name].include?('flaky')
+    end
+
     def gather_tags(element)
       return {} unless element.include? :tags
-      element[:tags].map { |tag| tag[:name][1..-1] unless tag[:name].include? '-fs' }.compact
+      element[:tags].map { |tag| tag[:name][1..-1] unless tag[:name].include?('-fs') || check_excluded_tag_condition(tag) }.compact
     end
 
     def gather_fs(element)
@@ -13,7 +17,7 @@ module GherkinLint
 
     def gather_tag_locations(element)
       return {} unless element.include? :tags
-      element[:tags].map { |tag| _get_tag_dict(tag) unless tag[:name].include? '-fs' }.compact
+      element[:tags].map { |tag| _get_tag_dict(tag) unless tag[:name].include?('-fs') || check_excluded_tag_condition(tag) }.compact
     end
 
     def gather_fs_locations(element)
